d7aea4ceb3f7543f93db703cf07e9a1d1488d354
carlos.perez <carlospverano@gmail.com>
2026-03-26 22:31:27 -0500

    finishing documentation with OpenAPI/Swagger

--- src/main/java/co/edu/uniquindio/controller/EmployeeController.java
+++ src/main/java/co/edu/uniquindio/controller/EmployeeController.java
@@ -5,6 +5,7 @@ import co.edu.uniquindio.dto.DepartmentDTO;
 import co.edu.uniquindio.dto.EmployeeDTO;
 import co.edu.uniquindio.service.IEmployeeService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -31,27 +32,60 @@ public class EmployeeController {
     }
 
 
-    @PostMapping
     @Operation(
-            summary = "Creating a employee",
-            description = "Creating a new employee"
+            summary = "Creating a new employee",
+            description = "Creating a new employee using the provided information"
     )
-    @ApiResponses({
+    @ApiResponses(value = {
             @ApiResponse(responseCode = "201", description = "Employee created",
                     content = @Content(mediaType = "application/json",
                     schema = @Schema(implementation = EmployeeDTO.class),
-                    examples = @ExampleObject(name = "Created", value = "{\"id\": 1, \"name\": \"Carlos\", \"position\": \"Developer.\"}")))
+                    examples = @ExampleObject(name = "Created", value = "{\"id\": 1, \"name\": \"Carlos\", \"position\": \"Developer.\"}"))),
+            @ApiResponse(responseCode = "400", description = "Bad request",
+                    content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = EmployeeDTO.class),
+                    examples = @ExampleObject(name = "Bad request", value = ""))),
+            @ApiResponse(responseCode = "500", description = "Internal server error",
+                    content = @Content)
     })
-    public ResponseEntity<EmployeeDTO> createEmployee(@Valid @RequestBody CreateEmployeeDTO createEmployeeDTO){
+    @PostMapping
+    public ResponseEntity<EmployeeDTO> createEmployee(
+            @Parameter(
+                    description = "Employee data to be created",
+                    required = true
+            )
+            @Valid
+            @RequestBody CreateEmployeeDTO createEmployeeDTO){
         EmployeeDTO employeeCreated = employeeService.createEmployee(createEmployeeDTO);
         return ResponseEntity.created(URI.create("/api/employees/" + employeeCreated.getId())).body(employeeCreated);
     }
-
+    @Operation(
+            summary = "Get employee by ID",
+            description = "Retrieves a specific employee using their unique identifier"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Employee successfully retrieved",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = EmployeeDTO.class)
+                    )),
+            @ApiResponse(responseCode = "404",
+                    description = "Employee not found",
+                    content = @Content),
+            @ApiResponse(responseCode = "500",
+                    description = "Internal server error",
+                    content = @Content)
+    })
     @GetMapping("/{id}")
-    public ResponseEntity<EmployeeDTO> queryEmployeeById(@PathVariable Long id){
+    public ResponseEntity<EmployeeDTO> queryEmployeeById(
+            @Parameter(
+                    description = "Unique identifier of the employee",
+                    required = true,
+                    example = "1"
+            )
+            @PathVariable Long id){
         EmployeeDTO employeeFound = employeeService.getEmployeeById(id);
         return ResponseEntity.ok(employeeFound);
-
-
     }
 }
